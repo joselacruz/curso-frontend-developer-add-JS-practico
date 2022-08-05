e68766e94ec5ad22edb204d71539da5d90300153
joselacruz <joselacruzperez@gmail.com>
2022-08-04 22:19:34 -0400

    Menu Mobile add

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktoMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 navEmail.addEventListener("click",toogleDesktopMenu);
+menuHamIcon.addEventListener("click", toogleMobileMenu);
+
 
 function toogleDesktopMenu () {
     desktoMenu.classList.toggle("inactive");
+}
+
+function toogleMobileMenu () {
+    mobileMenu.classList.toggle("inactive");
+    
 }
